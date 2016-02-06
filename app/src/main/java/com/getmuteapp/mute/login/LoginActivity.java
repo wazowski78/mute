3290c53f81864d520e23233990534c9fc49b78dd
ulasakdeniz <akdenizulas@gmail.com>
2016-02-06 17:12:57 +0200

    Login: user and friend requests sync

--- app/src/main/java/com/getmuteapp/mute/login/LoginActivity.java
+++ app/src/main/java/com/getmuteapp/mute/login/LoginActivity.java
@@ -6,12 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -19,10 +19,10 @@ import com.getmuteapp.mute.R;
 import com.getmuteapp.mute.services.CommunicationService;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -60,64 +60,15 @@ public class LoginActivity extends AppCompatActivity {
         facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(final LoginResult loginResult) {
-                //Log.d(LOG_TAG,"Success");
                 Log.d(LOG_TAG, "Access Token: " + loginResult.getAccessToken().getToken());
+
                 GraphRequest request = GraphRequest.newMeRequest(
                         loginResult.getAccessToken(),
-                        new GraphRequest.GraphJSONObjectCallback() {
-                            @Override
-                            public void onCompleted(JSONObject object, GraphResponse response) {
-                                GetFriendsThread thread = new GetFriendsThread(loginResult);
-                                thread.start();
-                                Thread main = Thread.currentThread();
-                                try {
-                                    main.sleep(3000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                                GraphResponse friendsResponse = thread.getGraphResponse();
-                                JSONObject friendsResponseJSONObject = friendsResponse.getJSONObject();
-
-                                try {
-                                    JSONArray friendsArray = friendsResponseJSONObject.getJSONArray("data");
-                                    StringBuilder sb = new StringBuilder();
-                                    sb.append("{");
-                                    sb.append("\"user\":{\"userId\":\"");
-                                    sb.append(object.getString("id"));
-                                    sb.append("\",\"facebookId\":\"");
-                                    sb.append(object.getString("id"));
-                                    sb.append("\",\"name\":\"");
-                                    sb.append(object.getString("name"));
-                                    sb.append("\",\"email\":\"");
-                                    sb.append(object.getString("email"));
-                                    sb.append("\",\"dateOfBirth\":\"");
-                                    sb.append(object.getString("birthday"));
-                                    sb.append("\",\"score\":0,\"gender\":\"");
-                                    sb.append(object.getString("gender"));
-                                    sb.append("\"},\"friends\":[");
-                                    for(int i = 0; i < friendsArray.length(); i++) {
-                                        JSONObject jsonObject = friendsArray.getJSONObject(i);
-                                        sb.append("\"");
-                                        sb.append(jsonObject.getString("id"));
-                                        sb.append("\"");
-                                        if(i != friendsArray.length()-1) {
-                                            sb.append(",");
-                                        }
-                                    }
-                                    sb.append("]}");
-                                    Log.d(LOG_TAG, "SERVER: " + sb.toString());
-                                    //Service triggered.
-                                    CommunicationService.startActionPostServer(context,sb.toString(),ACTION_LOGIN);
-
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-
+                        newMeRequestJsonCallback(loginResult)
+                        );
 
                 Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender, birthday");
+                parameters.putString("fields", "id,name,email,gender,birthday");
                 request.setParameters(parameters);
                 Log.d(LOG_TAG, parameters.toString());
                 request.executeAsync();
@@ -136,35 +87,64 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public class GetFriendsThread extends Thread {
-        private LoginResult loginResult;
-        private GraphResponse graphResponse;
+    private GraphRequest.GraphJSONObjectCallback newMeRequestJsonCallback(final LoginResult loginResult) {
+        return new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    //When newMeRequest completed getFacebookFriends
+                    //get() blocks the current thread and waits for result
+                    List<GraphResponse> friendsResponseList = getFacebookFriends(loginResult).get();
 
-        public GetFriendsThread(LoginResult loginResult) {
-            this.loginResult = loginResult;
-        }
+                    //friendsResponseList is a one-element List (I guess)
+                    GraphResponse friendsResponse = friendsResponseList.get(0);
+                    JSONObject friendsResponseJSONObject = friendsResponse.getJSONObject();
 
-        public void run() {
-            graphResponse = GraphRequest.newMyFriendsRequest(
-                    loginResult.getAccessToken(),
-                    new GraphRequest.GraphJSONArrayCallback() {
-                        @Override
-                        public void onCompleted(JSONArray objects, GraphResponse response) {
-                            Log.d(LOG_TAG, response.toString());
-                        /*for (int p = 0; p < objects.length(); p++) {
-                            try {
-                                JSONObject object = objects.getJSONObject(p);
-                                Log.d(LOG_TAG, object.toString());
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }*/
+                    JSONArray friendsArray = friendsResponseJSONObject.getJSONArray("data");
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("{");
+                    sb.append("\"user\":{\"userId\":\"");
+                    sb.append(object.getString("id"));
+                    sb.append("\",\"facebookId\":\"");
+                    sb.append(object.getString("id"));
+                    sb.append("\",\"name\":\"");
+                    sb.append(object.getString("name"));
+                    sb.append("\",\"email\":\"");
+                    sb.append(object.getString("email"));
+                    sb.append("\",\"dateOfBirth\":\"");
+                    sb.append(object.getString("birthday"));
+                    sb.append("\",\"score\":0,\"gender\":\"");
+                    sb.append(object.getString("gender"));
+                    sb.append("\"},\"friends\":[");
+                    for (int i = 0; i < friendsArray.length(); i++) {
+                        JSONObject jsonObject = friendsArray.getJSONObject(i);
+                        sb.append("\"");
+                        sb.append(jsonObject.getString("id"));
+                        sb.append("\"");
+                        if (i != friendsArray.length() - 1) {
+                            sb.append(",");
                         }
-                    }).executeAndWait();
-        }
+                    }
+                    sb.append("]}");
+                    Log.d(LOG_TAG, "SERVER: " + sb.toString());
+                    //Service triggered.
+                    CommunicationService.startActionPostServer(context, sb.toString(), ACTION_LOGIN);
 
-        public GraphResponse getGraphResponse() {
-            return graphResponse;
-        }
+                } catch (Exception ex) {
+                    Log.e(LOG_TAG, ex.getMessage());
+                }
+            }
+        };
+    }
+
+    private GraphRequestAsyncTask getFacebookFriends(LoginResult loginResult) {
+        return GraphRequest.newMyFriendsRequest(
+                loginResult.getAccessToken(),
+                new GraphRequest.GraphJSONArrayCallback() {
+                    @Override
+                    public void onCompleted(JSONArray objects, GraphResponse response) {
+                        Log.d(LOG_TAG, response.toString());
+                    }
+                }).executeAsync();
     }
 }
